548f7322af1e2ce9fa0621199ed144179b5edcb7
temirlan.utegenov <89874158170NtvbhkfY>
2021-09-17 21:41:23 +0300

    remarks

--- src/test/java/selenium/ConfProperties.java
+++ src/test/java/selenium/ConfProperties.java
@@ -11,7 +11,7 @@ public class ConfProperties {
     static {
         try {
 
-            fileInputStream = new FileInputStream(".\\src\\test\\resources\\conf.properties");
+            fileInputStream = new FileInputStream("./src/test/resources/conf.properties");
             PROPERTIES = new Properties();
             PROPERTIES.load(fileInputStream);
         } catch (IOException e) {
